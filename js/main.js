8a30f636228f6114eaca5c0be37f1e8ce32209a0
dufgr <dufigueiredo07@gmail.com>
2024-10-31 02:03:58 -0300

    Fix: correcting issues

--- js/main.js
+++ js/main.js
@@ -1,42 +1,42 @@
-$('#telefone').mascara('(00) 00000-0000', {
-    placeholder: '(DDD) 12345-6789'
-})
+$("#telefone").mask("(00) 00000-0000", {
+  placeholder: "(DDD) 12345-6789",
+});
 
-$('#cpf').mascara('000.000.000-00', {
-    placeholder: '123.456.789-00'
-})
+$("#cpf").mask("000.000.000-00", {
+  placeholder: "123.456.789-00",
+});
 
-$('#cep').mascara('00000-000', {
-    placeholder: '012345-678'
-})
+$("#cep").mask("00000-000", {
+  placeholder: "012345-678",
+});
 
-$('form').validate({
-    rules: {
-        nome: {
-            requerido: true
-        },
-        email: {
-            requerido: true,
-            email: true
-        },
-        telefone: {
-            requerido: true
-        },
-        endereco: {
-            requerido: true
-        },
-        cep: {
-            requerido: true
-        },
-        cpf: {
-            requerido: true
-        },
+$("form").validate({
+  rules: {
+    nome: {
+      required: true,
     },
-    submitHandler: function (form) {
-        alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
-        form.reset();
+    email: {
+      required: true,
+      email: true,
     },
-    invalidHandler: function (form, validator) {
-        alert("Por favor, preencha os campos para prosseguir com a compra!");
-    }
-})
+    telefone: {
+      required: true,
+    },
+    endereco: {
+      required: true,
+    },
+    cep: {
+      required: true,
+    },
+    cpf: {
+      required: true,
+    },
+  },
+  submitHandler: function (form) {
+    alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
+    form.reset();
+  },
+  invalidHandler: function (form, validator) {
+    alert("Por favor, preencha os campos para prosseguir com a compra!");
+  },
+});
